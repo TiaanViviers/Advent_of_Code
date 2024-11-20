67b3336df9f935e902a713600625decc86777d45
Tiaan Viviers <tiaanviv@gmail.com>
2024-11-20 15:44:36 +0200

    progress

--- 2023/day_11/solution_1.py
+++ 2023/day_11/solution_1.py
@@ -1,14 +1,23 @@
 import sys
 
 def expand(arr):
-    #expand cols
-
-
-    #expand rows
-    for i, row in enumerate(arr):
+    # Expand columns
+    transpose = [''.join(col) for col in zip(*arr)]
+    new_transpose = []
+    for i, row in enumerate(transpose):
+        new_transpose.append(row)
         if len(set(row)) == 1:
-            arr.insert(i+1, ['.' for _ in range(len(row))])
+            new_transpose.append('.' * len(row))
+    arr = [''.join(col) for col in zip(*new_transpose)]
 
+    # Expand rows
+    new_arr = []
+    for i, row in enumerate(arr):
+        new_arr.append(row)
+        if len(set(row)) == 1:
+            new_arr.append('.' * len(row))
+
+    return new_arr
 
 
 def main():
@@ -16,6 +25,11 @@ def main():
         input = f.read()
 
     univ = expand(input.split('\n'))
+    g_pos = get_galaxy_pos(univ)
+
+    sum = 0
+    for galaxy in g_pos:
+        
 
 
 if __name__ == '__main__':
